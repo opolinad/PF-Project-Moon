3446daec8754b96acfff69b58a8be706768a989e
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-13 20:07:24 -0300

    added redux few corrections

---
+++ front/src/App.jsx
@@ -0,0 +1,7 @@
+import { divide } from "lodash";
+import React from "react";
+
+export default function App()
+{
+    return(<div id="appCont">Hello World!</div>)
+}
--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, useDispatch } from 'react-redux';
+import store from "./redux/store.js";
+
+import App from "./App.jsx";
 
 ReactDOM.render(    
-  <Provider ><App/></Provider>,
+  <Provider store={store}><App/></Provider>,
       document.getElementById('root')
     );
