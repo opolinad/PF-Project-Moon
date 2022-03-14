2a8bafe1345b62e9e2d017c7ec38e146ba675662
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-14 20:37:04 -0300

    added navbar notifications menu
    Navbar: header que esta en todas las rutas, tiene el menu, notis y search
    Menu: tiene 3 links, a /user/self, /user/settings y /user/saved
    Notifications: Muestra una lista de las ultimas notificaciones

--- front/src/Navbar/Navbar.jsx
+++ front/src/Navbar/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Menu from "./components/Menu/Menu.jsx";
+import Notifications from "./components/Notifications/Notifications.jsx";
 
 export default function Navbar()
 {
+
+    const [showMenu,setShowMenu] = useState(false);
+    const [showNotifications,setShowNotifications] = useState(false);
+    const [search,setSearch] = useState("");
+
+    function searching()
+    {
+        
+    }
+
+    let menu;
+    let notifications;
+    if(showMenu) menu=<Menu/>
+    if(showNotifications) notifications=<Notifications/>;
     return(
-        <div id="navbarContainer">Navbar</div>
+        <div id="navbarShell">
+            <div id="menuIcon">
+                <div id="menuShell" onClick={()=>setShowMenu(!showMenu)}>Menu</div>
+                {menu}
+            </div>
+            
+            <div id="SearchContainer">
+                <div id="inputSearchShell"><input type="text" value={search} name="searchInput" id="searchInput" onChange={(e)=>setSearch(e.target.value)} placeholder={"Search"} /></div>
+                <div id="inputButtonShell"><button onClick={searching} id="searchButton">owo!</button></div>
+            </div>
+            <div id="notificationsShell">
+            <div id="notificationIcon" onClick={()=>setShowNotifications(!showNotifications)}>Notis</div>
+                {notifications}
+            </div>
+        </div>
     )
 }
