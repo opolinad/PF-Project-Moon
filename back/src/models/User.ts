8ccf72dad6f4c4372ba451f411f0d4a8dbf08f8f
lautarovollmer <lautarovollmer0@gmail.com>
2022-03-15 17:24:17 -0300

    models

---
+++ back/src/models/User.ts
@@ -0,0 +1,20 @@
+import {Schema,model} from 'mongoose'
+
+export interface User {
+    username: string;
+    email: string;
+    password: string;
+    image?: string;
+}
+
+const UserSchema = new Schema<User>(
+    {
+        username: {type:String,required:true,unique:true},
+        email: {type:String,unique:true},
+        password: {type:String,required:true},
+        image: {type:String},
+    },
+    {timestamps:true}
+    )
+
+module.exports = model<User>('User', UserSchema)
