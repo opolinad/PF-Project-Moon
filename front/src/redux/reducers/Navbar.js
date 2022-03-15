661512382a5ebc0e1646e9abfd972b6be5c675ef
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-15 18:33:04 -0300

    added navbar functional

---
+++ front/src/redux/reducers/Navbar.js
@@ -0,0 +1,16 @@
+import { GET_NOTIFICATIONS, GET_NOTIFICATIONS_LOADING, LOADING_0, SEARCHING_ACTION } from "../consts";
+
+
+
+export function searchReducer(state="",action)
+{
+    if(action.type===SEARCHING_ACTION){return action.payload}
+    return state;
+}
+
+export function notifications(state={status:LOADING_0,posts:[]},action)
+{
+    if(action.type===GET_NOTIFICATIONS_LOADING){return {status:LOADING_0,posts:[]};}
+    else if(action.type===GET_NOTIFICATIONS){return action.payload;}
+    else return state;
+}
