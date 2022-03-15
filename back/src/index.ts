6e7e2ab9b194702c966bb27bb86a82c21bb018c1
lautarovollmer <lautarovollmer0@gmail.com>
2022-03-15 15:26:10 -0300

    base de datos

---
+++ back/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+
+import cors from 'cors'
+
+import dotenv from 'dotenv'
+
+import { dbConnect } from './db'
+const app = express()
+
+// const allRoute = require( './routes/routes')
+
+dotenv.config()
+
+app.use(cors())
+
+app.use(express.json())
+
+// app.use('/api',allRoute)
+
+app.listen(3001, ()=> {
+    dbConnect()
+    console.log('Server on PORT 3001')
+})
--- back/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const res = require('express/lib/response');
-const fetch = require("node-fetch");
-//const { Sequelize } = require('sequelize/types');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-//const { /* models */ } = require('../db.js');
-
-const router = Router();
