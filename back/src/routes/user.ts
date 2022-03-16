bb32f6d37aa8a1c9922fe6dfb4291aff203d02fb
neriheredia <developer.heredia@gmail.com>
2022-03-16 11:22:33 -0300

    verify

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,13 +1,20 @@
 import { Router, Request, Response } from 'express'
+import { verifyToken } from '../helpers/verifyToken'
 const User = require('../models/User')
 const router = Router()
+const CryptoJS = require('crypto-js')
 
-router.put('/:id', async (req:Request,res:Response) => {
-    const { id } = req.params
-
+router.put('/:id',verifyToken, async (req:Request,res:Response) => {
+    const { password } = req.body
+    if(password){
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.HASH_CRYPTO
+        ).toString()
+    }
     try {
         const putUser = await User.findByIdAndUpdate(
-            id, 
+            req.params.id, 
             {
                 $set: req.body
             },
