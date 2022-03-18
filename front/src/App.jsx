6abaa2516f2276e3f55760e37fc4ff0249782d4e
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-18 20:24:13 -0300

    finished navbar-css

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,13 @@
 import React, { Fragment } from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from "./Home/Home.jsx";
-import LandingPage from "./LandingPage/LandingPage.jsx";
-import Post from "./Post/Post.jsx";
-import Register from "./Register/Register.jsx";
-import User from "./User/User.jsx";
-import Messages from "./Messages/Messages.jsx";
-import UserBoard from "./User/UserBoard.jsx";
-import NotFound from "./NotFound/NotFound";
+
 import "./App.css"
+import Navbar from "./Navbar/Navbar.jsx";
 
 export default function App()
 {
   return(      
     <div id="appCont">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path= '/'>
             <Route index element={<LandingPage/>}/>
@@ -24,7 +17,8 @@ export default function App()
             <Route path="*" element={<NotFound/>} />
           </Route>  
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Navbar/>
     </div>
   )
 }
--- protocol/src/Navbar/components/Menu/Menu.jsx
+++ protocol/src/Navbar/components/Menu/Menu.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MenuCss from "./Menu.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faCog, faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Menu(props)
 {
     return(
-        <div id="MenuCont">
+        <div id={MenuCss.MenuCont}>
             {/* <div className="menuOptionShell"><Link to={"/user/self"} className="menuOption">Account</Link></div>
             <div className="menuOptionShell"><Link to={"/user/settings"} className="menuOption">Settings</Link></div>
             <div className="menuOptionShell"><Link to={"/user/favorites"} className="menuOption">Favorites</Link></div> */}
-            <div className="menuOptionShell"><p  className="menuOption">Account</p></div>
-            <div className="menuOptionShell"><p  className="menuOption">Settings</p></div>
-            <div className="menuOptionShell"><p  className="menuOption">Favorites</p></div>
+            <div className={MenuCss.menuOptionShell}> <p className={MenuCss.menuImg}><FontAwesomeIcon icon={faHome}/></p> <p  className={MenuCss.menuOption}>Home</p></div>
+            <div className={MenuCss.menuOptionShell}> <p className={MenuCss.menuImg}><FontAwesomeIcon icon={faUser}/></p> <p  className={MenuCss.menuOption}>Account</p></div>
+            <div className={MenuCss.menuOptionShell}> <p className={MenuCss.menuImg}><FontAwesomeIcon icon={faCog}/></p> <p  className={MenuCss.menuOption}>Settings</p></div>
+            <div className={MenuCss.menuOptionShell}> <p className={MenuCss.menuImg}><FontAwesomeIcon icon={faBookmark}/></p> <p  className={MenuCss.menuOption}>Favorites</p></div>
         </div>
     )
 }
