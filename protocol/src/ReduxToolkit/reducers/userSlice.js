1c1fa46a4308b1fa0f69e847ef14055c3ee0cdb5
lautarovollmer <lautarovollmer0@gmail.com>
2022-03-21 15:47:17 -0300

    form

--- protocol/src/ReduxToolkit/reducers/userSlice.js
+++ protocol/src/ReduxToolkit/reducers/userSlice.js
@@ -1,30 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: "userData",
-    initialState: {
-        currentUser: null,
-        isFetching: false,
-        error: false
+  name: "userData",
+  initialState: {
+    currentUser: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    userStart: (state) => {
+      state.currentUser = null;
+      state.isFetching = true;
+      state.error = false;
     },
-    reducers: {
-        userStart: (state) => {
-            state.currentUser = null
-            state.isFetching = true
-            state.error = false
-        },
-        userSuccess: (state, action) => {
-            state.currentUser = action.payload
-            state.isFetching = false
-            state.error = false
-        },
-        userFailure: (state) => {
-            state.currentUser = null
-            state.isFetching = false
-            state.error = true
-        },
-    }
-})
+    userSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isFetching = false;
+      state.error = false;
+    },
+    userFailure: (state) => {
+      state.currentUser = null;
+      state.isFetching = false;
+      state.error = true;
+    },
+  },
+});
 
 export const { userStart, userSuccess, userFailure } = userSlice.actions;
 export default userSlice.reducer;
