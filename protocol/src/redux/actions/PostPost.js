d877e9319114614ec4ecaffa0eebfecc367b4b63
Lucas Gastaldi <lucas-gastaldi@hotmail.com>
2022-03-21 13:48:44 -0300

    PostPost, action, reducer

---
+++ protocol/src/redux/actions/PostPost.js
@@ -0,0 +1,25 @@
+import { POST_POST, GET_CAT } from "../consts.js";
+import "regenerator-runtime/runtime";
+import axios from 'axios';
+
+
+export function postPost(userId){
+    return async function(dispatch) {
+        const res = await axios.post(`http://localhost:3001/api/posts/${userId}`, payload)
+        dispatch({ 
+            type: POST_POST, 
+            payload: res.data 
+        })
+    }
+}
+
+export function getCategories(){
+    return async function(dispatch){
+        var json = await axios.get('http://localhost:3001/categories')
+        return dispatch({
+            type: GET_CAT,
+            payload: json.data
+        })
+    }
+}
+
