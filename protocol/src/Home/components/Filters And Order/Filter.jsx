08e8d7bbfcb10c6bd5de441d262ff8bae2c16d19
Daniel Andrés López Sabogal <andreslop@gmail.com>
2022-03-23 16:05:54 -0500

    Filter working, still making request to wrong endpoint (not ready yet)

--- protocol/src/Home/components/Filters And Order/Filter.jsx
+++ protocol/src/Home/components/Filters And Order/Filter.jsx
@@ -9,14 +9,13 @@ export default function Filter()
 
     function filtering(e)
     {
-        alert("Entra")
         dispatch(setFeedToLoading())
-        dispatch(setFilter(e.target.value))
+        dispatch(setFilter(e.target.options[e.target.selectedIndex].value))
     }
 
     return(
         <div id={FilterCss.filterCont}>
-            <select name="filterSelect" id={FilterCss.filterSelect} onChange={(e)=>filtering(e)}>
+            <select name="filterSelect" id={FilterCss.filterSelect} onChange={filtering}>
                 <option value="" className={FilterCss.filterOption}>All</option>
                 {/* <option value="trending" className="filterOption">Trending</option> */}
                 <option value="designsOnly" className={FilterCss.filterOption}>Designs Only</option>
