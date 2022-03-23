385daa9f254cd989b6a9494448aaf2c8c41bcfdf
Daniel Andrés López Sabogal <andreslop@gmail.com>
2022-03-23 11:43:22 -0500

    filter actios fixed, but filter isn't working

--- protocol/src/Home/components/Filters And Order/Filter.jsx
+++ protocol/src/Home/components/Filters And Order/Filter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-
+import { setFeedToLoading, setFilter } from "../../../ReduxToolkit/reducers/homeSlice";
 import FilterCss from "./Filter.module.css";
 
 export default function Filter()
@@ -9,13 +9,14 @@ export default function Filter()
 
     function filtering(e)
     {
-        //dispatch(Actions.setFeedToLoading())
-        //dispatch(Actions.changeFilter(e.target.value))
+        alert("Entra")
+        dispatch(setFeedToLoading())
+        dispatch(setFilter(e.target.value))
     }
 
     return(
         <div id={FilterCss.filterCont}>
-            <select name="filterSelect" id={FilterCss.filterSelect} onChange={(e)=>filtering(e)}>  
+            <select name="filterSelect" id={FilterCss.filterSelect} onChange={(e)=>filtering(e)}>
                 <option value="" className={FilterCss.filterOption}>All</option>
                 {/* <option value="trending" className="filterOption">Trending</option> */}
                 <option value="designsOnly" className={FilterCss.filterOption}>Designs Only</option>
