461bbfb0535bb34b3b2334d6dca828f73e3c7149
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-22 22:42:49 -0300

    Draft 22.42 22/3/22

--- protocol/src/CardPost/CardPost.jsx
+++ protocol/src/CardPost/CardPost.jsx
@@ -3,6 +3,7 @@ import Cardpost from "./CardPost.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from "react-router-dom";
 import {faHeart,faShareSquare, faCommentAlt} from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
 /*
     title: string,
     description: string,
@@ -16,32 +17,36 @@ import {faHeart,faShareSquare, faCommentAlt} from "@fortawesome/free-solid-svg-i
 //Likes y shares: son arrays de ids, tengo que usar .length para la cantidad y buscar la id de user para saber si le dio like xd.
 export default function CardPost(props)
 {
+    const user = useSelector(state=>state.user);
+
 
     let cardValues={}
     
     props.description? cardValues.description=props.description : cardValues.description="";
     
-    if(props.imgs){cardValues.imgs=props.imgs.map((element,index)=><img key={"img_"+index} className={Cardpost.cardpostImg} src={element} alt={"nu existe :c"}/>)}
+    if(props.imgs.length){cardValues.imgs=<div className={Cardpost.imgsCont}>{props.imgs.map((element,index)=><img key={"img_"+index} className={Cardpost.cardpostImg} src={element} alt={"nu existe :c"}/>)}</div>}
     
-    props.liked ? cardValues.likeImg=<FontAwesomeIcon className={Cardpost.notLikedImg} icon={faHeart}/> : cardValues.likeImg=<FontAwesomeIcon className={Cardpost.likedImg} icon={faHeart} /> ; 
-    props.shared ? cardValues.sharedImg=<FontAwesomeIcon className={Cardpost.notSharedImg} icon={faShareSquare} /> : cardValues.sharedImg=<FontAwesomeIcon className={Cardpost.sharedImg}  icon={faShareSquare} /> ; 
+    props.likes.includes(user.currentUser?._id)?   cardValues.likeImg=Cardpost.likedImg : cardValues.likeImg=Cardpost.notLikedImg; 
+    props.shares.includes(user.currentUser?._id) ? cardValues.sharedImg=Cardpost.sharedImg : cardValues.sharedImg=Cardpost.notSharedImg; 
     
-    cardValues.likes=props.likes;
-    cardValues.shares=props.shares;
+    cardValues.categories=<div id={Cardpost.categoriesCont}>{props.categories.map((element,index)=><p key={"cardpost_"+props.postId+"_category"+index} className={Cardpost.category}>{element}</p>)}</div>
+    cardValues.likes=props.likes.length;
+    cardValues.shares=props.shares.length;
     cardValues.favorite=props.favorite;
 
     return(
         <div className={Cardpost.CardPostCont}>
             <div className={Cardpost.userInfoCont}>
-                <img className={Cardpost.userPhoto} src={props.userPhoto} alt=":c" />
+                <img className={Cardpost.userPhoto} src={props.userPhoto? props.userPhoto : "./default_profile_photo.svg"} alt="not_found" />
                 <Link to={"http://localhost:3000/user/"+props.userId} className={Cardpost.userName}>{props.userName}</Link>
             </div>
             <h2 className={Cardpost.cardPostTitle}>{props.title}</h2>
             <div className={Cardpost.descriptionCont}><p className={Cardpost.cardPostDescription}>{cardValues.description}</p></div>
-            <div className={Cardpost.imgsCont}>{cardValues.imgs}</div>
+            {cardValues.imgs}
+            {cardValues.categories}
             <div className={Cardpost.analiticsCont}>
-                <div className={Cardpost.likesShell} onClick={()=>{}}>{cardValues.likeImg}{cardValues.likes}</div>
-                <div className={Cardpost.sharesShell} onClick={()=>{}}>{cardValues.sharedImg}{cardValues.shares}</div>
+                <div className={Cardpost.likesShell} onClick={()=>{}}> <FontAwesomeIcon className={Cardpost.notLikedImg} icon={faHeart}/> {cardValues.likes}</div>
+                <div className={Cardpost.sharesShell} onClick={()=>{}}> <FontAwesomeIcon className={Cardpost.notSharedImg} icon={faShareSquare} /> {cardValues.shares}</div>
                 <div className={Cardpost.favoritesShell}>{cardValues.favorite}</div>
                 <div className={Cardpost.commentShell}><Link to={"http://localhost:3000/post/"+props.postId}><FontAwesomeIcon icon={faCommentAlt}/>  Commentaries</Link></div>
             </div>
