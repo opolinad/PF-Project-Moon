1d9762021046c80bded4402a04549649a7c033df
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-27 19:34:04 -0500

    Update postCall.js

--- protocol/src/ReduxToolkit/apiCalls/postCall.js
+++ protocol/src/ReduxToolkit/apiCalls/postCall.js
@@ -7,12 +7,12 @@ import { SUCCESS_200 } from "../consts";
 import { userPostsSuccess } from "../reducers/usersPosts";
 
 export const getCategories = async (dispatch) => {
-    var json = await axios.get('http://localhost:3001/api/categories')
+    var json = await axios.get('/api/categories')
     dispatch(json.data)
 }
 
 export const getDetailedPost = async (id,dispatch) => {
-    const res = await axios.get(`http://localhost:3001/api/posts/${id}`);
+    const res = await axios.get(`/api/posts/${id}`);
     let out={};
     res.data.hasOwnProperty("_id") ? out=res.data : out={error:true};
     console.log(out);
@@ -21,7 +21,7 @@ export const getDetailedPost = async (id,dispatch) => {
 
 export const postPost = async (dispatch, userId, input, token) => {
     try {
-        const res = await axios.post(`http://localhost:3001/api/posts/${userId}`, input, {
+        const res = await axios.post(`/api/posts/${userId}`, input, {
             headers: {
                 token
             }
@@ -36,7 +36,7 @@ export const postPost = async (dispatch, userId, input, token) => {
 export const deletePost=async(dispatch, postId, token, postArray, father)=>{
     try {
         let pos = postArray.indexOf(postArray.filter(post=>post._id===postId)[0]);
-        await axios.delete(`http://localhost:3001/api/posts/${postId}`, { headers:{token}})
+        await axios.delete(`/api/posts/${postId}`, { headers:{token}})
         let newArr=[...postArray];
         newArr.splice(pos,1)
         father==="Feed" && dispatch(feedDatabase({status:SUCCESS_200, posts:newArr }))
@@ -48,7 +48,7 @@ export const deletePost=async(dispatch, postId, token, postArray, father)=>{
 
 export const sendBackComment = async (id,comment,dispatch) => {
     try {
-        //const res = await axios.post(`http://localhost:3001/api/posts/comment/${id} `, comment)
+        //const res = await axios.post(`/api/posts/comment/${id} `, comment)
         console.log("PostCall")
         dispatch(setNewComment(comment))
 
