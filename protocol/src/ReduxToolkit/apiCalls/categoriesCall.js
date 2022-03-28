b3115b88d5e09545402be70a9ee56164c6f1cf0f
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-27 19:30:33 -0500

    Update categoriesCall.js

--- protocol/src/ReduxToolkit/apiCalls/categoriesCall.js
+++ protocol/src/ReduxToolkit/apiCalls/categoriesCall.js
@@ -3,7 +3,7 @@ import { NOT_FOUND_404, SUCCESS_200 } from "../consts";
 import { getCategories } from "../reducers/homeSlice";
 
 export let getCategoriesAsync = async (dispatch) => {
-    let resp = await axios.get("http://localhost:3001/api/categories");
+    let resp = await axios.get("/api/categories");
     let status = NOT_FOUND_404;
     if (resp.data.categories.length) { status = SUCCESS_200 }
     dispatch(getCategories({status, post:resp.data}));
