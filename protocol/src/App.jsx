e07af0cac59e58070fae2605057244a0102d24d8
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-27 20:03:55 -0500

    Update App.jsx

--- protocol/src/App.jsx
+++ protocol/src/App.jsx
@@ -11,7 +11,7 @@ import NotFound from "./NotFound/NotFound";
 import Navbar from "./Navbar/Navbar.jsx";
 import CookiesPolicy from "./CookiesPolicy/CookiesPolicy.jsx";
 import PostPost from "./PostPost/PostPost"
-import axios from "axios"
+// import axios from "axios"
 import "./App.css";
 import UserEdit from "./User/UserEdit.jsx";
 import Bandeja from "./Conversations/Bandeja.jsx";
