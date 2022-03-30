7ac352f86ba385a18810f785299102db0a96575a
Daniel Andrés López Sabogal <andreslop@gmail.com>
2022-03-30 11:40:11 -0500

    fixed login w/ platforms (at least in local environment)

--- back/src/index.ts
+++ back/src/index.ts
@@ -15,7 +15,7 @@ app.use(passport.session());
 app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
