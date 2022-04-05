b3034ebdb7584d8ec12d577be48811fa1355219c
lautarovollmer <lautarovollmer0@gmail.com>
2022-04-05 16:48:33 -0300

    check

--- protocol/src/User/Posts/UserPosts.jsx
+++ protocol/src/User/Posts/UserPosts.jsx
@@ -1,40 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import css from "./UserPosts.module.css"
-import { allPostById } from '../../ReduxToolkit/apiCalls/userPostsById';
-import { useState } from 'react';
-import CardPost from '../../CardPost/CardPost'
+import css from "./UserPosts.module.css";
+import { allPostById } from "../../ReduxToolkit/apiCalls/userPostsById";
+import { useState } from "react";
+import CardPost from "../../CardPost/CardPost";
 
 export default function Posts() {
-    // const {user} = useParams()
-    const user = useSelector(state => state.user.currentUser);
-    const currentUser = useSelector(state => state.userData)
-    const posts = useSelector(state => state.userPostsById.posts);
-    const dispatch = useDispatch()
-    const [loading, setLoading] = useState(true)
+  // const {user} = useParams()
+  const user = useSelector((state) => state.user.currentUser);
+  const currentUser = useSelector((state) => state.userData.currentUser);
+  const posts = useSelector((state) => state.userPostsById.posts);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        allPostById(dispatch, user._id, currentUser?._id)
-            .then(res => {
-                setLoading(false)
-            })
-    }, [dispatch, user])
+  useEffect(() => {
+    allPostById(dispatch, user._id, currentUser._id).then((res) => {
+      console.log(currentUser);
+      setLoading(false);
+    });
+  }, [dispatch, user]);
 
-    const URL = useLocation();
-    let displaying = posts
+  const URL = useLocation();
+  let displaying = posts;
 
-    return (
-        <div id={css.container}>
-            {
-                loading ? (<p>loading...</p>) : 
-                (
-                <>
-                    {displaying && displaying?.map(data => (<CardPost key={data._id} title={data.title} shared={data.share} shareUser={data.shareUser} description={data.description} imgs={data.images} shares={data.shares} likes={data.likes} id={data._id} userName={data.user.username} userPhoto={data.user.profilePhoto} userId={data.user._id} componentFather={"UserPost"}/>))}
-                </>
-                )
-            }
-            <button>LOAD MORE</button>
-        </div>
-    )
+  return (
+    <div id={css.container}>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          {displaying &&
+            displaying?.map((data) => (
+              <CardPost
+                key={data._id}
+                title={data.title}
+                shared={data.share}
+                shareUser={data.shareUser}
+                description={data.description}
+                imgs={data.images}
+                shares={data.shares}
+                likes={data.likes}
+                id={data._id}
+                userName={data.user.username}
+                userPhoto={data.user.profilePhoto}
+                userId={data.user._id}
+                componentFather={"UserPost"}
+              />
+            ))}
+        </>
+      )}
+      <button>LOAD MORE</button>
+    </div>
+  );
 }
