4834d72282ee59c91e417aa60ea9329b235774ec
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-04-05 00:16:00 -0500

    Update login.ts

--- back/src/routes/login.ts
+++ back/src/routes/login.ts
@@ -34,7 +34,7 @@ router.post('/', async (req: Request, res: Response) => {
 router.get("/session", async (req: Request, res: Response) => {
   try {
     console.log("Correo",email)
-    const user = await User.find({ email });
+    const user = await User.find();
     console.log("Usuario encontrado",user);
     const { ...others } = user._doc;
     const accessToken = jwt.sign({ id: user._id }, process.env.JWT_KEY, { expiresIn: '1d' });
