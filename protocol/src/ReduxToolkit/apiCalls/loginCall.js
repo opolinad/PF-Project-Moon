d7aab1306d28c97afe50d6974ff60b1ab6ec6860
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-04-04 22:34:19 -0500

    Update loginCall.js

--- protocol/src/ReduxToolkit/apiCalls/loginCall.js
+++ protocol/src/ReduxToolkit/apiCalls/loginCall.js
@@ -10,7 +10,7 @@ export const loginUser = async (dispatch, user) => {
     try {
         let res;
         if (user.hasOwnProperty("platform")) {
-            res = await axios.get(`/api/login/session/`, { withCredentials: true });
+            res = await axios.get(`/api/login/session/`);
         } else {
             res = await axios.post(`/api/login/`, user)
         }
